231ae9bf75f2e79ccba0bf41825cc3e2d5cee8fe
bramirez96 <bran.ramirez.don@gmail.com>
2024-04-18 02:17:34 -0700

    Delete any evidence of code.

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,3 @@
 // ! Copyright (c) 2024, Brandon Ramirez, brr.dev
 
-import { API } from "./api";
-
-const api = new API();
-
-export default api;
-export { API };
-export * from "./api.types";
+export {};
